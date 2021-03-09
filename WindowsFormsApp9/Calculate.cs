162c58965689a83e95ad1fa3efec1dcde63f6692
Ella <Ella@DESKTOP-0JLJP6K>
2021-03-09 10:51:42 +0330

    class

---
+++ WindowsFormsApp9/Calculate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp9
+{
+    class Calculate
+    {
+        int a;
+        public Calculate()
+        {
+            a = 10;     //mitonim be moteghayer ozve class 
+                       //dakhele sazande meghdar dehi konim
+
+            //num1 = 5;     ERROR
+        }
+        //tafavot a va num1: a moteghayer ozve classe va hame tavabe mitonan
+        //azash estefade konan vali num1 faghat dar tavabe tarif shhode ast
+        //bekhatere hamin nemitonim kharej az tabe estefade konim
+        //chon kharej az tabe tarif nashode
+
+        //bayad sathe dastresi ro public dar nazar
+        //begirim ke ghable estefade bashe dide beshe
+        
+        
+        /// <summary>
+        /// add two string numbers
+        /// </summary>
+        /// <param name="a"> string num1 </param>
+        /// <param name="b"> string num2 </param>
+        /// <returns> an int value</returns>
+        public int sum(string a, string b)
+        {
+            int num1 = Convert.ToInt32(a);
+            int num2 = Convert.ToInt32(b);
+
+            return num1 + num2;
+        }
+        /// <summary>
+        /// add two string numbers
+        /// </summary>
+        /// <param name="st"> st[0] num1 , st[1] num2 </param>
+        /// <returns></returns>
+        public int sum(string[] st)
+        {
+            int num1 = Convert.ToInt32(st[0]);
+            int num2 = Convert.ToInt32(st[1]);
+
+            return num1 + num2;
+        }
+        /// <summary>
+        /// add two int numbers
+        /// </summary>
+        /// <param name="a"> int num1 </param>
+        /// <param name="b"> int num2 </param>
+        /// <returns></returns>
+        public  int sum(int a, int b)
+        {
+            int sum = a + b;
+
+            return sum;
+           // return a + b;     ya intori
+        }
+
+        int diff(int a, int b)
+        {
+            int d;
+            d = a - b;
+            return d;
+        }
+
+
+    }
+}
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Calculate cal = new Calculate();
+            string[] s = { textBox1.Text, textBox2.Text };
+            label3.Text = cal.sum(s).ToString();
+            
+
+
+            //label3.Text = cal.sum(textBox1.Text, textBox2.Text).ToString();
+            /*
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            label3.Text = cal.sum(num1, num2).ToString();
+            */
+        }
+    }
+}
